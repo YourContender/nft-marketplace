b952dab37f3350c5ca94fffdedb3255b504d8b0e
YourContender <dream-boy123@ukr.net>
2024-10-09 01:44:41 +0300

    add styles for header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,31 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import logo from "../../img/logo.png";
 import user from "../../img/user.png";
 import "./header.scss";
 
 export const Header: FC = () => {
-  return (
-    <header className="header">
-        <div className="header_logo">
-            <img src={logo} alt="logo" />
-            <span>NFT Marketplace</span>
-        </div>
+    const [openBurger, setOpenBurger] = useState(false);
 
-        <div className="header_nav">
-            <span className="header_nav-item">Marketplace</span>
-            <span className="header_nav-item">Rankings</span>
-            <span className="header_nav-item">Connect a wallet</span>
-            <button className="header_nav-btn">
-                <img src={user} alt="user" />
-                <span>Sign in</span>
-            </button>
-        </div>
-    </header>
-  )
+    return (
+        <header className="header">
+            <div className="header_logo">
+                <img src={logo} alt="logo" />
+                <span>NFT Marketplace</span>
+            </div>
+
+            <div className="header_nav">
+                <span className="header_nav-item">Marketplace</span>
+                <span className="header_nav-item">Rankings</span>
+                <span className="header_nav-item">Connect a wallet</span>
+                <button className="header_nav-btn">
+                    <img src={user} alt="user" />
+                    <span>Sign in</span>
+                </button>
+            </div>
+
+            <div className={openBurger ? "header_burger active" : "header_burger"} onClick={() => setOpenBurger(!openBurger)}>
+                <span></span>
+            </div>
+        </header>
+    )
 }
